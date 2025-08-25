04096c509b82dd5a9335b56ce2dfa65091158fbe
Cristiane Mayabanza <152671012+itscristianee@users.noreply.github.com>
2025-08-25 14:55:37 +0200

    Criação ASP.NET Core MVC project com Identity (.NET 8)

---
+++ PlataformaCursosOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PlataformaCursosOnline.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
